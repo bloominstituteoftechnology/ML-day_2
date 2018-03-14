141c50fdb79265a8ef674788f9f72fd889e98e68
Amethyst <oconnellamethyst@gmail.com>
2018-03-14 02:35:11 -0500

    Lecture 2 Stuff

---
+++ LectureNotesDay2/example1.py
@@ -0,0 +1,14 @@
+# Visualization
+# Data imports
+# Build simple models
+# Use powerful APIs
+# Tensorflow
+# Execute Models Remotely
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([3,2,1])
+
+plt.plot(x)
+plt.show()
---
+++ LectureNotesDay2/example2.R
@@ -0,0 +1,1 @@
+plot(c(3,2,1),type="l",col="blue")
